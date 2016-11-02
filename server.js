4f79b2a15006b69aa8e11f8f27a2f036575789e7
ToasterTom <tate.thomas5595@gmail.com>
2016-11-01 18:39:54 -0600

    config file

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = module.exports = express();
-var massive = require('massive');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var config = require('./config');
+
+app.use(session({secret: config.secret}));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+  clientID: config.id,
+  clientSecret: config.secret,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function(token, refreshToken, profile, done) {
+  return done(null, profile);
+}));
 
 
 
-
-
-
-
-app.listen('3737', function(){
-  console.log("Successfully listening on : 3737")
+app.listen( config.port, function(){
+  console.log("Successfully listening")
 })
